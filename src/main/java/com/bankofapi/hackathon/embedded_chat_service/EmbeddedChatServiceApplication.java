9c722f33d8ba5f44d17d548a9ea0b508e7b3609a
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-07-26 20:16:39 +0530

    initial commit for setting up skeleton

---
+++ src/main/java/com/bankofapi/hackathon/embedded_chat_service/EmbeddedChatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bankofapi.hackathon.embedded_chat_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbeddedChatServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankofapi/hackathon/embedded_chat_service/EmbeddedChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankofapi.hackathon.embedded_chat_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbeddedChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
