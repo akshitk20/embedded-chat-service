369d4496c4c48d5bc4091e69deeb717bcdd8c04e
Akshit Khatri <akshitkhatri.20@gmail.com>
2025-01-13 08:53:02 +0530

    completed RAG implementation

--- src/main/java/com/bankofapi/hackathon/embedded_chat_service/service/ChatService.java
+++ src/main/java/com/bankofapi/hackathon/embedded_chat_service/service/ChatService.java
@@ -5,7 +5,18 @@ import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ChatService {
@@ -18,17 +29,28 @@ public class ChatService {
 
     private final DirectAccessAccountsService directAccessAccountsService;
 
-    public ChatService(ChatModel chatModel, CitizenshipVerificationService citizenshipVerificationService, LoanQuotesService loanQuotesService, DirectAccessAccountsService directAccessAccountsService) {
+    private final VectorStore vectorStore;
+
+    @Value("classpath:/prompts/natwest-reference.st")
+    private Resource resource;
+
+    public ChatService(ChatModel chatModel, CitizenshipVerificationService citizenshipVerificationService, LoanQuotesService loanQuotesService, DirectAccessAccountsService directAccessAccountsService, VectorStore vectorStore) {
         this.chatClient = ChatClient.builder(chatModel)
                 .defaultAdvisors(new PromptChatMemoryAdvisor(new InMemoryChatMemory()))
                 .build();
         this.citizenshipVerificationService = citizenshipVerificationService;
         this.loanQuotesService = loanQuotesService;
         this.directAccessAccountsService = directAccessAccountsService;
+        this.vectorStore = vectorStore;
     }
 
     public String getChatResponse(String message) {
-        return chatClient.prompt()
+        Map<String, Object> map = new HashMap<>();
+        map.put("input", message);
+        map.put("documents", String.join("/n", findSimilarDocuments(message)));
+        PromptTemplate promptTemplate = new PromptTemplate(resource, map);
+        Prompt prompt = promptTemplate.create();
+        return chatClient.prompt(prompt)
                 .function("CitizenshipVerificationService", "Check citizenship details for the user", citizenshipVerificationService)
                 .function("DirectAccessAccountsService", "Accounts access", directAccessAccountsService)
                 .function("LoanQuoteService", "Check Loan", loanQuotesService)
@@ -36,4 +58,13 @@ public class ChatService {
                 .call()
                 .content();
     }
+
+    private List<String> findSimilarDocuments(String message) {
+        List<Document> documents = vectorStore.similaritySearch(SearchRequest.query(message).withTopK(2));
+        return documents.stream()
+                .map(Document::getContent)
+                .toList();
+    }
+
+
 }
